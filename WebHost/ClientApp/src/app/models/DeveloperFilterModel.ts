3cd6d84d28138d6fa1a6baa066b2158d84612176
Andrew <a13andrew@gmail.com>
2019-04-30 18:19:38 +0300

    Added DeveloperFilterModel

---
+++ WebHost/ClientApp/src/app/models/DeveloperFilterModel.ts
@@ -0,0 +1,18 @@
+export class DeveloperFilterModel {
+    
+    public sort: string;
+
+    public order: string;
+
+    public skip: number;
+
+    public take: number;
+
+    public keywords: string;
+
+    public set: string;
+
+    public projectContextUrl: string;
+
+    constructor() { }
+}
